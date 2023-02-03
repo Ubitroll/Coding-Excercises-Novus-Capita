6ca0391e43da680b32116bfd7664b73e9fe85dbb
Ubitroll <stephencartner@hotmail.co.uk>
2023-02-03 11:41:22 +0000

    Problem 1 Completed

---
+++ CodingExcercises/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace CodingExcercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sales = { 1, 2, 1, 1, 0, 3, 1, 0, 0, 2, 4, 1, 0, 0, 0, 0, 2, 1, 0, 3, 1, 0, 0, 0, 6, 1, 3, 0, 0, 0 };
+
+            int count = 1;
+            int max = 1;
+
+            for (int i = 1; i < sales.Length; i++)
+            {
+                if (sales[i] == sales[i-1])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > max)
+                    {
+                        max = count;
+                    }
+                    count = 1;
+                }
+            }
+
+            if (count > max)
+            {
+                Console.WriteLine("The longest chain is {0} long", count);
+            }
+            else
+            {
+                Console.WriteLine("The longest chain is {0} long", max);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
