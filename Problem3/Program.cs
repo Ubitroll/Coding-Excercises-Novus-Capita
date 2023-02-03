9b2fe612965954eb9922086a3c9991ae534ea890
Ubitroll <stephencartner@hotmail.co.uk>
2023-02-03 14:44:28 +0000

    Problem 3 Completed

---
+++ Problem3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Problem3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, space, star;
+
+            for (row = 0; row < 5; row++)
+
+            {
+
+                for (space = 0; space < 5 - row; space++)
+
+                {
+
+                    Console.Write(" ");
+
+                }
+
+                for (star = 0; star < 1 + row + row; star++)
+
+                {
+
+                    Console.Write("*");
+
+                }
+
+                Console.WriteLine();
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }    
+}
