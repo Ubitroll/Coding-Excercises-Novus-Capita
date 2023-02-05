03e9bb27bc4f69ef4de611eedb07ab5911f00069
Ubitroll <stephencartner@hotmail.co.uk>
2023-02-05 21:18:53 +0000

    Problem 9 Completed

---
+++ Problem9/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Problem9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of primes in the sequence you would like: ");
+
+            int searchedPrime = Convert.ToInt32(Console.ReadLine());
+
+            int currentNumber = 1;
+            int count = 0;
+
+            while (count != searchedPrime)
+            {
+                currentNumber++;
+                if (CheckIfPrime(currentNumber))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"The prime number in position {searchedPrime} in the sequence of prime numbers is {currentNumber}");
+
+            Console.ReadLine();
+        }
+
+        static bool CheckIfPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            if (number <= 3)
+            {
+                return true;
+            }
+
+            if (number % 2 == 0 || number % 3 == 0)
+            {
+                return false;
+            }
+
+            for (int i = 5; i * i <= number; i = i + 6)
+            {
+                if (number % i == 0 || number % (i + 2) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
