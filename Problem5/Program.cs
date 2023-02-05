84812be5ac0576b29d2d090a6a778bea575e56db
Ubitroll <stephencartner@hotmail.co.uk>
2023-02-05 15:14:27 +0000

    Problem 5 Completed

---
+++ Problem5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace Problem5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string to be swapped around: ");
+
+            string input = Console.ReadLine();
+
+            char[] inputArray= input.ToCharArray();
+
+            string output = "";
+
+            for (int i = inputArray.Length; i > 0; i--)
+            {
+                output = output + inputArray[i-1];
+            }
+
+            Console.WriteLine("The reversed string is: ");
+
+            Console.WriteLine(output);
+
+            Console.ReadLine();
+        }
+    }
+}
